9dcb2e8141a3d0177fdbab22be30c69f77740d64
blogscot <iain.diamond@gmail.com>
2017-02-02 18:28:31 +0000

    Refactor benchmarking

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -7,35 +7,45 @@ defmodule Benchmark do
   multiple times, saving the timing results to log file.
   """
 
-  @doc """
-  Runs and displays the time taken for  the TSP algorithm.
-  """
+  @algorithm &Tsp.run/0
 
-  def measure(run, file) do
-    (fn -> Cellular.Tsp.run end)
+  # Measures the time taken to execute the given function.
+
+  defp measure(function) do
+    function
     |> :timer.tc
     |> elem(0)
     |> Kernel./(1_000_000)
-    |> (fn duration ->
+  end
+
+  # Log the test run details to file.
+
+  def log_to_file(duration, run, file) do
       {{year, month, day}, {hour, mins, secs}} = :calendar.local_time
-      text = "Run #{run}: (#{day}/#{month}/#{year} #{hour}:#{pad(mins)}:#{pad(secs)}) The algorithm ran in #{duration} seconds."
+      time_info = "(#{day}/#{month}/#{year} #{hour}:#{pad(mins)}:#{pad(secs)})"
+      text = "Run #{pad(run)}: #{time_info} The algorithm ran in #{duration} seconds."
       IO.puts(text)
       IO.puts(file, text)
-    end).()
+  end
+
+  defp pad(number) do
+    number
+    |> Integer.to_string
+    |> String.pad_leading(2, "0")
   end
 
   @doc """
   Executes the measure function the number of times given by count.
   """
-
-  def run(count \\ 1) do
-
+  def run(count \\ 30) do
     {:ok, file} = File.open(@logfile, [:read, :utf8, :append])
-    for run <- 1..count, do: measure(run, file)
+
+    for run <- 1..count do
+      measure(@algorithm)
+      |> log_to_file(run, file)
+    end
+
     File.close(file)
   end
-
-  defp pad(number) when number < 10, do: "0" <> Integer.to_string(number)
-  defp pad(number), do: number
 
 end
