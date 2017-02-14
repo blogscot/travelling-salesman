aa0e143582f496989792f9f091b7a07eae219c44
blogscot <iain.diamond@gmail.com>
2017-02-14 21:00:34 +0000

    Add extra Crossover benchmark

--- bench/crossover_bench.exs
+++ bench/crossover_bench.exs
@@ -7,6 +7,10 @@ defmodule CrossoverBench do
 
   def pop100, do: Population.new(100)
   def pop10, do: Population.new(10)
+  def distributed_pop do
+    pop100()
+    |> Enum.take(6)
+  end
 
   bench "Crossover and mutate a population of 10", [population: pop10()] do
     population
@@ -16,6 +20,13 @@ defmodule CrossoverBench do
   end
 
   bench "Crossover and mutate a population of 100", [population: pop100()] do
+    population
+    |> GeneticAlgorithm.crossover(100, @crossover_rate, @tournament_size)
+    |> GeneticAlgorithm.mutate(@mutation_rate)
+    nil  # required by Benchfella so that return value is deterministic
+  end
+
+  bench "Distributed population", [population: distributed_pop()] do
     population
     |> GeneticAlgorithm.crossover(100, @crossover_rate, @tournament_size)
     |> GeneticAlgorithm.mutate(@mutation_rate)
