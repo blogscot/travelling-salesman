ad12c635f4694b5e53e6e6258ed31a38e4b66835
blogscot <iain.diamond@gmail.com>
2017-03-18 13:29:40 +0000

    Split Benchmark.run into individual algorithm functions.

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -5,37 +5,55 @@ defmodule Benchmark do
   Performs basic benchmarking activities, i.e. running the TSP algorithms
   multiple times, saving the timing results to log file.
   """
-  @algorithm_under_test "Island"
-
-  # @algorithm  &Tsp.MasterSlave.run/0
-  # @log_params Tsp.MasterSlave.get_log_params()
-
-  # @algorithm (fn -> Tsp.Island.run(num_workers) end)
-  @log_params Tsp.Island.get_log_params()
-
-  # For cellular model config {2,2} the num_workers should be set to 2 as 2 processes are
-  # spawned per node and the grid contains 4 processes in total.
-  # @algorithm (fn -> Tsp.Cellular.run(num_workers, {2,2}) end)
-  # @log_params Tsp.Cellular.get_log_params()
 
   @doc """
-  Executes the measure function the number of times given by count.
+  Executes the Master Slave Model algorithm using the given number of workers.
+  The algorithm is repeated n times, specified by the count parameter.
   """
-  def run(num_workers, count \\ 30) do
-    display_start_message(num_workers)
+  def run_master_slave(num_workers, count \\ 30) do
+    display_start_message("MasterSlave", num_workers, Tsp.MasterSlave.get_log_params())
 
     for run <- 1..count do
-      # Logger.info("Run #{run}: The algorithm ran in #{measure(fn -> Tsp.Cellular.run(num_workers, {2,3}) end)} seconds.")
+      Logger.info("Run #{run}: The algorithm ran in #{measure(fn -> Tsp.MasterSlave.run(num_workers) end)} seconds.")
+    end
+  end
+
+  @doc """
+  Executes the Island Model algorithm using the given number of workers.
+  The algorithm is repeated n times, specified by the count parameter.
+  """
+  def run_island(num_workers, count \\ 30) do
+    display_start_message("Island", num_workers, Tsp.Island.get_log_params())
+
+    for run <- 1..count do
       Logger.info("Run #{run}: The algorithm ran in #{measure(fn -> Tsp.Island.run(num_workers) end)} seconds.")
     end
   end
 
-  defp display_start_message(num_workers) do
+  @doc """
+  Executes the Cellular Model algorithm using the given number of workers.
+  The algorithm is repeated n times, specified by the count parameter.
+
+  Valid grid dimensions:
+  For each connected node num_workers will be spawned. Thus, for two
+  workers each running on two connected nodes the total number of workers is four.
+  Therefore, the number of workers in the grid should equal four. E.g. a {2,2}
+  grid has valid dimensions.
+  """
+  def run_cellular(num_workers, {row, col}, count \\ 30) do
+    display_start_message("Cellular", num_workers, Tsp.Cellular.get_log_params())
+
+    for run <- 1..count do
+      Logger.info("Run #{run}: The algorithm ran in #{measure(fn -> Tsp.Cellular.run(num_workers, {row,col}) end)} seconds.")
+    end
+  end
+
+  defp display_start_message(algorithm_name, num_workers, log_params) do
     {{year, month, day}, _} = :calendar.local_time
     num_nodes = Cluster.number_nodes
-    Logger.info("#{@algorithm_under_test} algorithm test, started on #{day}/#{month}/#{year} " <>
+    Logger.info("#{algorithm_name} algorithm test, started on #{day}/#{month}/#{year} " <>
       "using #{num_nodes}x#{num_workers} worker processes")
-    Logger.info(@log_params)
+    Logger.info(log_params)
   end
 
   # Measures the time taken to execute the given function.
